6a2b48541f68078a7a516f857c4f2d19d6593418
Frank Raso <frankraso91@gmail.com>
2022-03-28 20:57:23 -0400

    Submit

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -138,3 +138,41 @@ console.log(`I wasn't exactly sure if you meant an array created by the input of
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+
+  function adc(value) {
+	let voltage = 5;
+	let output = (voltage / 1023) * value;
+	return +output.toFixed(2);
+}
+
+console.log(adc(0));
+console.log(adc(1023));
+console.log(adc(400));
+console.log(adc(500));
+
+//Here we use a mathimatical formula to create a virtual DAC
+// otherwise known as a digital to analog converter commonly 
+//found on music playback devices like mp3 players iphones and laptops etc.
+//first we name the function and give it a parameter so that we can convert whatever value we put into the function
+//into volts. first we create a variable voltage and set it to 5 so that we can use it in the next part of the argument.
+//next we create a variable named output and we set its value to be voltage divided by 1023 which is our value range 
+//and then multiplied by the actual value input.We fix the ouput to 2 places after the decimal with .tofixed(2)
+//below that we try some values and check to see if they are correct and they are!
+// this code couldve have been written easier by eliminating the concrete parts that never change. 
+//example: how about instead of having 4 lines of code we just write 
+
+
+
+ function adc(value) {
+	return +(value / (1023 / 5  )).toFixed(2)
+ }
+ console.log(adc(0));
+ console.log(adc(1023));
+ console.log(adc(400));
+ console.log(adc(500));
+
+
+// It is alot easier to explain the long way but I prefer the shoter code as it yields
+// the same results without the redundancy!
+
